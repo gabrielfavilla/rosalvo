5cdf648f0b32f92e088ce51f0261cfd765c8764e
Gabriel Favilla Gugelmin <gb.fg@hotmail.com>
2017-03-30 15:07:54 -0300

    Commit Inicial

---
+++ assets/js/js.js
@@ -0,0 +1,97 @@
+$(function(){
+  clickOutsideMenu();
+
+  // MENU
+  // click no hamburguer icon
+  $('.MenuTrigger').on('click', function(e){
+    e.preventDefault();   
+
+    if( $('.Menu').hasClass('Menu--open') ){
+      closeMenu();
+    } else{
+      openMenu();
+    }
+  });
+
+  // abre um sub menu
+  $('.Menu--hasSub > a').on('click', function(e){
+    e.preventDefault();
+
+    $(this).siblings('.Menu-sub').addClass('Menu-sub--subOpen');
+    $(this).addClass('is-selected');
+    $('.Menu').addClass('Menu--subOpen');
+  });
+
+  // volta ao menu principal
+  $('.js-back').on('click', function(){
+    $(this).parent().removeClass('Menu-sub--subOpen');
+    $('.Menu--hasSub > a').removeClass('is-selected');
+    $('.Menu').removeClass('Menu--subOpen');
+  });
+
+  // menu fixo ao scollar
+  $(window).scroll(function(){
+    
+    if( $(this).scrollTop() >= 100 ){
+      $('.Header').addClass('is-scrolling');
+    } else{
+    	$('.Header').removeClass('is-scrolling');
+    }
+  });
+
+});
+
+function closeMenu(){
+  $('.Menu').removeClass('Menu--open');
+  $('.Menu').removeClass('Menu--subOpen');
+
+  $('.Menu-sub').removeClass('Menu-sub--subOpen');
+  $('.MenuTrigger').removeClass('is-open');
+
+  $('.Menu--hasSub > a').removeClass('is-selected');
+
+  $('body').removeClass('overflowHidden');
+}
+
+function openMenu(){
+  $('.MenuTrigger').addClass('is-open');
+  $('.Menu').addClass('Menu--open');
+  $('body').addClass('overflowHidden');
+}
+
+function viewport() {
+  var e = window, a = 'inner';
+  if (!('innerWidth' in window )) {
+      a = 'client';
+      e = document.documentElement || document.body;
+  }
+  return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
+}
+
+function checkWindowWidth(){
+  var w = viewport().width;
+  var size = '';
+  if(w > 991){
+    size = 'desktop';
+  } else{
+    size = 'mobile';
+  }
+
+  return size;
+}
+
+function clickOutsideMenu(){
+  
+  var x = checkWindowWidth();
+  if(x == 'desktop'){
+    $(document).on('mouseup', function (e){ 
+      var elem = $('.Menu-sub');
+
+      if (!elem.is(e.target) && elem.has(e.target).length === 0){
+        closeMenu();
+      }
+    });
+  } else{
+    $(document).off('mouseup');
+  }
+}
