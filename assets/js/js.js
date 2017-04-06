24380a4694595b19e9c60d92bf0fc4c43689ee16
Gabriel Favilla Gugelmin <gb.fg@hotmail.com>
2017-04-06 18:00:02 -0300

    Versão Inicial

--- assets/js/js.js
+++ assets/js/js.js
@@ -39,6 +39,106 @@ $(function(){
     }
   });
 
+
+  // validar formulario
+  $('.js-submitForm').on('click', function (e) {
+
+    e.preventDefault();
+    var qtdErro = 0;
+
+    $(this).parents('.Form').find('[data-validate=true]').each(function () {
+      var value = $.trim($(this).find('input, textarea').val());
+      if (!value.length > 0) {
+        $(this).addClass('error');
+        qtdErro++;
+      }
+    });
+
+    if (qtdErro == 0) {
+      return $.ajax({
+        type: "POST",
+        url: "/ajax/contato.php",
+        data: $(this).serialize(),
+        success: function (data) {
+          if (data === "success") {
+            console.log('Mensagem enviada com sucesso.');
+            // Limpa o form
+            $('.Form').trigger("reset");
+          } else {
+            console.log('Erro ao tentar enviar mensagem: ' + data);
+          }
+        }
+      });
+    } else {
+      console.log('Erro ao tentar enviar mensagem. Tente novamente.');
+    }
+  });
+
+
+  // MAPS
+
+  function initialize() {
+
+      var chicago = new google.maps.LatLng(-19.890709, -43.920771);
+      var myOptions = {
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          center: chicago,
+          mapTypeControl: false,
+          scrollwheel: false
+      };
+      map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+      markerMuseu = new google.maps.Marker({
+        position: new google.maps.LatLng(-19.890709, -43.920771),
+        map: map,
+        title: 'Museu do Automóvel'
+      });
+
+      markerMuseu.addListener('click', function() {
+        infowindow.open(map, markerMuseu);
+      });
+  }
+
+  initialize();
+
+
+  // SLIDER PRODUTO
+
+  $('.js-productSlider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.SliderNav__nav',
+    prevArrow: '<button type="button" class="Slider__button Slider__button--prev"></button>',
+    nextArrow: '<button type="button" class="Slider__button Slider__button--next"></button>',
+    fade: true
+  });
+
+  $('.js-productNav').slick({
+    arrows: false,
+    asNavFor: '.Slider',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    centerMode: true,
+    infinite: true,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      }
+    ]
+  });
+
+  // SCROLLBAR
+  $('.u-scrollbar').perfectScrollbar(); 
+
 });
 
 function closeMenu(){
